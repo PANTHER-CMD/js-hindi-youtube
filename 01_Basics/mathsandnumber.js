83e5752037aeedeeedbb2c643ece15005ae1e405
PANTHER-CMD <143009960+PANTHER-CMD@users.noreply.github.com>
2024-12-09 19:55:27 +0000

    maths

---
+++ 01_Basics/mathsandnumber.js
@@ -0,0 +1,22 @@
+const score = 400
+const balance = new Number(400);
+console.log(score)
+console.log(balance)
+console.log(balance.toString())
+
+const othernum = 12.564
+console.log(othernum.toPrecision(4))
+
+
+const num = 100000023
+console.log(num.toLocaleString('en-IN'));
+
+
+//+++++++++++maths+++++++++++
+
+// convert negative to positive but not vice versa
+console.log(Math.abs(-51))
+
+console.log(Math.round(5.12))
+console.log(Math.min(5,6,8,3,4,7,23,4))
+console.log(Math.max(5,6,8,3,4,7,23,4))
